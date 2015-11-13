58f49abad963e69e9a6a65b23c09d58a552c07dc
Amy McPherson <qcricket@gmail.com>
2015-11-13 08:55:15 -0800

    Initialized run_Analysis.R.

---
+++ run_analysis.R
@@ -0,0 +1,14 @@
+## run_analysis.R collects data, cleans it, and makes a short summary
+## Read in dataset
+fileURL <- https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+## Merge the training and the test sets to create one data set.
+
+## Extract only the measurements on the mean and standard deviation for each measurement. 
+
+## Use descriptive activity names to name the activities in the data set
+
+## Appropriately label the data set with descriptive variable names. 
+
+## From the data set in the last step, create a second, independent tidy data 
+## set with the average of each variable for each activity and each subject.
